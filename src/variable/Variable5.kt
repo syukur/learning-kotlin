6df7825d3751457f5af726b15575a542fb0ea3a0
syukur.kurnia@dwidasa.com <syukur.kurnia@dwidasa.com>
2026-03-30 05:49:48 +0700

    variable : basic types

---
+++ src/variable/Variable5.kt
@@ -0,0 +1,11 @@
+package variable
+
+fun main() {
+    val d : Int
+    d = 3
+
+    val e : String = "hello"
+
+    println(d)
+    println(e)
+}
---
+++ src/variable/Variable6.kt
@@ -0,0 +1,10 @@
+package variable
+
+fun main() {
+    val a: Int = 100
+    val b = "log_message"
+    val c = 3.14
+    val d = 100_000_000_000_000
+    val e = false
+    val f = '\n'
+}
