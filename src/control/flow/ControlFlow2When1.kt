3c013668dfab43dbc8a79024da3ba7bae4a1d71e
syukur.kurnia@dwidasa.com <syukur.kurnia@dwidasa.com>
2026-04-02 06:06:53 +0700

    Control Flow: When completed

--- src/control/flow/ControlFlow2When1.kt
+++ src/control/flow/ControlFlow2When1.kt
@@ -2,7 +2,7 @@ package control.flow
 
 fun main() {
 
-    //  Example 1
+    //  Example 1 : Basic When
     val obj = "Hello"
 
     when (obj) {
@@ -10,4 +10,30 @@ fun main() {
         "Hello" -> println("Greeting")
         else -> println("Unknown")
     }
+
+
+    // Example 2 : When as expression
+    val result = when (obj) {
+        "1" -> "One"
+        "Hello" -> "Greeting"
+        else -> "Unknown"
+    }
+
+    println("result: $result")
+
+    /** Example 3 :
+     * When as an expression,
+     * When without subject
+      */
+
+    val trafficLightState = "Yellow"
+
+    val trafficAction = when {
+            trafficLightState == "Green" -> "Go"
+            trafficLightState == "Yellow" -> "Slow down"
+            trafficLightState == "Red" -> "Stop"
+            else -> "Malfunction"
+    }
+
+    println("trafficAction: $trafficAction")
 }
