dbd74792940ce11d7fb5afdbcae6c9aa28435520
syukur.kurnia@dwidasa.com <syukur.kurnia@dwidasa.com>
2026-04-02 13:38:22 +0700

    Control Flow: Exercise For finish

---
+++ src/control/flow/ControlFlow8ForExercise2.kt
@@ -0,0 +1,8 @@
+package control.flow
+
+fun main() {
+    val words = listOf("dinosaur", "limousine", "magazine", "language")
+    for (word in words)
+        if (word.startsWith("l"))
+            println(word)
+}
