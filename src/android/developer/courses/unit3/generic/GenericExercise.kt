66c85b2ebfed9c1d47e3c34e853eb230895d9c13
syukur.kurnia@dwidasa.com <syukur.kurnia@dwidasa.com>
2026-07-05 18:56:10 +0700

    android: extention function

--- src/android/developer/courses/unit3/generic/GenericExercise.kt
+++ src/android/developer/courses/unit3/generic/GenericExercise.kt
@@ -59,6 +59,17 @@ class Quiz(){
     }
 }
 
+val Quiz.StudentProgress.progressText: String
+    get() = "${answered} of $total answered"
+
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
+    println(Quiz.progressText)
+}
+
 fun main() {
-    println("${Quiz.answered} of ${Quiz.total} answered.")
+   // println("${Quiz.answered} of ${Quiz.total} answered.")
+    Quiz.printProgressBar()
 }
